94e60599cacdd2e8b22beb8f8daf53b323d77b21
Yuri_1967 <9022368@mail.ru>
2021-07-31 17:43:46 +0500

    Задание 1

---
+++ Lesson_1/1.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+
+url = 'https://api.github.com'
+user = 'softicer-67'
+
+
+request = requests.get(f'{url}/users/{user}/repos')
+
+js = request.json()
+for i in range(0, len(js)):
+    res = f'Project Number: {i + 1}\nProject Name: {js[i]["name"]}\nProject URL: {js[i]["svn_url"]}'
+    print(res)
