7e87b87859dc3be6816ab43d60bc987bf091c639
Yuri <57116431+softicer-67@users.noreply.github.com>
2021-08-16 20:07:55 +0500

    Add files via upload

---
+++ lesson5/settings.py
@@ -0,0 +1,11 @@
+
+BOT_NAME = 'lesson5'
+
+SPIDER_MODULES = ['lesson5.spiders']
+NEWSPIDER_MODULE = 'lesson5.spiders'
+
+USER_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
+
+ROBOTSTXT_OBEY = False
+
+DOWNLOAD_DELAY = 0
